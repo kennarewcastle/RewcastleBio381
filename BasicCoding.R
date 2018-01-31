c37499c24a5bd8f5d9c995ae456028637ed2b959
Kenna Rewcastle <kenna.rewcastle@gmail.com>
2018-01-31 13:48:19 -0500

    Basic coding R script, and practice compiling code to an html using notebook icon.

---
+++ BasicCoding.R
@@ -0,0 +1,11 @@
+# Basic R commands and usage
+# 30 January 2018
+# KER
+
+## Using the assignment operator
+x<-5 # preferred assignment syntax
+print(x)
+# command + shift + return will run the whole script at once.
+y = 4 # legal but not used excet in function 
+
+plantHeight<-5.5
