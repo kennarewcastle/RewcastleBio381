aad155b6b722a602f14caff706d7b1e018808557
Kenna Rewcastle <kenna.rewcastle@gmail.com>
2018-03-21 15:10:36 -0400

    Edited data exploration to work with PLFA instead of CO2 data.

--- Explore_CR_Dat.R
+++ Explore_CR_Dat.R
@@ -1,3 +1,16 @@
+#### Playing with d13C PLFA data from Costa Rica
+library(dplyr)
+CRdat<-read.csv("mean_dat.csv")
+CRdat$core_type<-as.factor(CRdat$core_type)
+str(CRdat)
+L1<-filter(CRdat,core_type==1)
+L1<-filter(L1,label_type=="L")
+L2<-filter(CRdat,core_type==2)
+L2<-filter(L2,label_type=="L")
+L3<-filter(CRdat,core_type==3)
+L3<-filter(L3,label_type=="L")
+
+
 #### Playing with d13 CO2 data from Costa Rica
 library(dplyr)
 dat<-read.csv("Costa Rica Master sheet annotated.csv")
